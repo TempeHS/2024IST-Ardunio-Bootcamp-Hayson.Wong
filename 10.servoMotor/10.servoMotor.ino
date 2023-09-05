82b96df52264c960951902643c30a5015e454f9b
Choooooper <hayson.wong@education.nsw.gov.au>
2023-09-05 18:06:02 +1000

    worked on bootcamp 10

--- 10.servoMotor/10.servoMotor.ino
+++ 10.servoMotor/10.servoMotor.ino
@@ -18,14 +18,36 @@
     https://github.com/TempeHS/TempeHS_Ardunio_Boilerplate/blob/main/Ardunio_Bootcamp/10.servoMotor/Bootcamp-servoMotor.png
 */
 
-//The Servo-h library is built into Ardunio IDE there is no need to side load it like the Ultrasonic Sensor library
+#include <Servo.h>
+#include "Ultrasonic.h"
 
+Servo myServo;
+static unsigned int myServoPin = 7;
+static unsigned int myUSPin = 6;
+unsigned long rangeInCM;
+
+Ultrasonic myUSsensor(myUSPin);
 
 void setup() {
-
+Serial.begin(9600);
+//Serial.println("This SM is 9600");
+myServo.attach(myServoPin);
+myServo.write(0);
+  delay(100);
 }
 
 // The loop function runs over and over again forever
 void loop() {
+
+  readUS();
+ 
+  if (rangeInCM <= 25){
+  openGate();
+  } else {
+  closeGate();
+  }
+
+  
+  
 
 }
---
+++ 10.servoMotor/Functions.ino
@@ -0,0 +1,17 @@
+void openGate() {
+  for (int i = 0; i <= 255; i++){
+    myServo.write(i);
+    delay(45);
+
+  }
+}
+void closeGate() {
+myServo.write(0);
+delay(1000);
+}
+
+void readUS(){
+  rangeInCM = myUSsensor.MeasureInCentimeters();
+  Serial.println(rangeInCM);
+
+}
