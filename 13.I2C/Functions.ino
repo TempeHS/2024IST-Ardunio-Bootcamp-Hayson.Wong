40ec54a4e3997f44e4d3ce56edc33bed4a7415bc
Choooooper <hayson.wong@education.nsw.gov.au>
2023-09-12 10:33:59 +1000

    update 13

---
+++ 13.I2C/Functions.ino
@@ -0,0 +1,11 @@
+void updateOLED (float theTemp) {
+  u8g2.clearBuffer();                  // clear the internal memory
+  u8g2.setFont(u8g2_font_ncenB08_tr);  // choose a suitable font
+  u8g2.setCursor(0, 10);              // Set cursor to first line
+  u8g2.print("HELLO WORLD!");         // write something to the internal memory
+  u8g2.setCursor(0, 20);              // Set cursor to first line
+  u8g2.print("the temprature is:");         // write something to the internal memory
+    u8g2.print(theTemp);         // write something to the internal memory
+  u8g2.sendBuffer();                  // transfer internal memory to the display
+
+}
